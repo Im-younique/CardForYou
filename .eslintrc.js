c18f44d3c4ce56e4433cb78008f7088e5050e4a5
Im-younique <dyyim4725@gmail.com>
2024-06-10 11:14:16 +0900

    [Init] Project 초기 세팅

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugin: [
+    'prettier'
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  }
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,13 @@
 module.exports = {
-  arrowParens: 'avoid',
+  arrowParens: 'always',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  trailingComma: es5,
+  endOfLine: lf
 };
